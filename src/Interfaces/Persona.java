2a628a0663d03804e19223ec73803e1b7e8c617c
max <maxmolotov@hotmail.it>
2017-05-17 13:22:56 +0200

    Primi passi

--- src/Interfaces/Persona.java
+++ src/Interfaces/Persona.java
@@ -10,11 +10,14 @@ public class Persona {
     private Genders gender;
     private int age = 0;
     private Type type;
+    private boolean occupato = false;
 
     public Genders getGender() { return gender; }
     public Type getType() { return type; }
     public int getAge() { return age; }
+    public boolean getOccupato() { return occupato; }
 
     public void setGender(Genders g) { gender = g; }
     public void setType(Type t) { type = t; }
+    public void setOccupato(boolean b) { occupato = b; }
 }
--- src/Interfaces/Popolazione.java
+++ src/Interfaces/Popolazione.java
@@ -1,7 +1,20 @@
 package Interfaces;
 
+import People.Femmina;
+import People.Maschio;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by max on 17/05/17.
  */
 public class Popolazione {
+    private List<Maschio> maschi = new ArrayList<>();
+    private List<Femmina> femmine = new ArrayList<>();
+
+    public void addMale(Maschio m) { maschi.add(m); }
+    public void addFemale(Femmina f) { femmine.add(f); }
+
+
 }
